703e049e67e2f5acb7ab41fe341657e0c706b071
zhaotianff <zhaotianff@163.com>
2021-09-05 17:54:39 +0800

    basic set background

--- Silver-Arowana/Silver-Arowana.Shell/Pages/StaticWallpaper.xaml.cs
+++ Silver-Arowana/Silver-Arowana.Shell/Pages/StaticWallpaper.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Silver_Arowana.Shell.Controls;
 
 namespace Silver_Arowana.Shell.Pages
 {
@@ -21,6 +22,8 @@ namespace Silver_Arowana.Shell.Pages
     /// </summary>
     public partial class StaticWallpaper : Page
     {
+        IEnumerable<string> recentWallpapers;
+
         public StaticWallpaper()
         {
             InitializeComponent();
@@ -28,14 +31,68 @@ namespace Silver_Arowana.Shell.Pages
 
         private void Page_Loaded(object sender, RoutedEventArgs e)
         {
-            StringBuilder sb = new StringBuilder(DesktopTool.MAX_PATH);
+            LoadCurrentBackground();
+            LoadRecentBackground();
+        }
+
+        private void LoadCurrentBackground()
+        {
+            var sb = new StringBuilder(DesktopTool.MAX_PATH);
             if (DesktopTool.GetBackground(sb))
             {
-                this.img_background.Source = new BitmapImage(new Uri(sb.ToString(), UriKind.Absolute));
+                SetPreviewImage(sb.ToString());
             }
+        }
 
-            sb = new StringBuilder(1024);
+        private void SetPreviewImage(string path)
+        {
+            this.img_background.Source = new BitmapImage(new Uri(path, UriKind.Absolute));
+        }
+
+        private void LoadRecentBackground()
+        {
+            wrap_wallpaper.Children.Clear();
+            var sb = new StringBuilder(1024);
             DesktopTool.GetRecentBackground(sb);
+            recentWallpapers = sb.ToString().Split(";").Take(5);
+
+            foreach (var wallpaper in recentWallpapers)
+            {
+                ThumbImageControl thumbImageControl = new ThumbImageControl();
+                thumbImageControl.ThumbPath = wallpaper;
+                thumbImageControl.Click += OnChangeBackground;
+                wrap_wallpaper.Children.Add(thumbImageControl);
+            }           
+        }
+
+        private void OnChangeBackground(object sender,EventArgs args)
+        {
+            var path = (sender as ThumbImageControl)?.ThumbPath;
+
+            if (System.IO.File.Exists(path) == false)
+                return;
+
+            if (SetBackground(path) == false)
+                return;
+
+            SetPreviewImage(path);
+
+            var index = recentWallpapers.ToList().IndexOf(path);
+            if (index == 0)
+                return;
+
+            var firstThumbImage = wrap_wallpaper.Children[0] as ThumbImageControl;
+            var firstThumbImagePath = firstThumbImage.ThumbPath;
+            firstThumbImage.ThumbPath = path;
+            (wrap_wallpaper.Children[index] as ThumbImageControl).ThumbPath = firstThumbImagePath;
+        }
+
+        private bool SetBackground(string path)
+        {
+            //设置壁纸
+            StringBuilder sb = new StringBuilder(PInvoke.DesktopTool.MAX_PATH);
+            sb.Append(path);
+            return PInvoke.DesktopTool.SetBackground(sb);               
         }
     }
 }
--- Silver-Arowana/Silver-Arowana.Test/Silver-Arowana.Test.cpp
+++ Silver-Arowana/Silver-Arowana.Test/Silver-Arowana.Test.cpp
@@ -7,6 +7,5 @@
 
 int main()
 {
-	//TestSetBackground();
- 	GetRecentBackground();
+	TestSetBackground();	
 }
