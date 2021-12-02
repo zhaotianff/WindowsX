3b372391e1060202c5208e691d479b6dfb226cb9
zhaotianff <zhaotianff@163.com>
2021-12-02 11:41:00 +0800

    wait message box

---
+++ Silver-Arowana/Silver-Arowana.Shell.Test/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using Silver_Arowana.Shell.MessageBoxEx;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Silver_Arowana.Shell.Test
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var result = await WaitMessageBox.Show("标题", "内容", "保留");
+            MessageBox.Show(result.ToString());
+        }
+    }
+}
---
+++ Silver-Arowana/Silver-Arowana.Shell/MessageBoxEx/WaitMessageBoxWindow.xaml.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Silver_Arowana.Shell.MessageBoxEx
+{
+    /// <summary>
+    /// Interaction logic for WaitMessageBoxWindow.xaml
+    /// </summary>
+    public partial class WaitMessageBoxWindow : Window
+    {
+        #region static field
+        private static AutoResetEvent autoResetEvent = new AutoResetEvent(false);
+        private static bool dialogResult = false;
+        private static WaitMessageBoxWindow waitMessageBoxWindow;
+        private static bool isActiveExit = false;
+        #endregion
+
+        #region instance field
+        private DispatcherTimer timer = new DispatcherTimer();
+        private int leftSecond = 0;
+        #endregion
+
+        #region instance property
+        public string ConfirmText { get; set; }
+      
+        public int LeftSecond
+        {
+            get => leftSecond;
+            set
+            {
+                leftSecond = value;
+                btn_Ok.Content = ConfirmText + $"({leftSecond}秒)";
+            }
+        }
+
+        public string ConfirmTimeText
+        {
+            get => btn_Ok.Content.ToString();
+            set
+            {
+                Dispatcher.Invoke(() =>
+                {
+                    btn_Ok.Content = ConfirmText + value;
+                });
+            }
+        }
+        #endregion
+
+        #region instance func
+
+        public WaitMessageBoxWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void StartTime(int second)
+        {
+            LeftSecond = second;
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        public void StopTimer()
+        {
+            timer.Stop();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            LeftSecond--;
+            if (LeftSecond == 0)
+            {
+                isActiveExit = true;
+                dialogResult = true;
+                autoResetEvent.Set();
+            }
+        }
+
+        private void BlurWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (isActiveExit == false)
+            {
+                dialogResult = false;
+                autoResetEvent.Set();
+            }
+        }
+
+        private void btn_Ok_Click(object sender, RoutedEventArgs e)
+        {
+            isActiveExit = true;
+            dialogResult = true;
+            autoResetEvent.Set();
+        }
+
+        private void btn_Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            isActiveExit = true;
+            dialogResult = false;
+            autoResetEvent.Set();
+        }
+
+        public void SetMessageBox(string title,string content)
+        {
+            lbl_Title.Content = title;
+            tbk_Content.Text = content;
+        }
+
+        #endregion
+
+        #region static func
+
+        public static async Task<bool> Show(string title, string content, string confirmText,int second)
+        {
+            waitMessageBoxWindow = new WaitMessageBoxWindow();
+            waitMessageBoxWindow.SetMessageBox(title, content);
+            waitMessageBoxWindow.ConfirmText = confirmText;
+            waitMessageBoxWindow.ConfirmTimeText = $"({second}秒)";
+            waitMessageBoxWindow.Show();
+            waitMessageBoxWindow.StartTime(second);
+
+            Task<bool> task = Task.Run(() => 
+            {
+                autoResetEvent.WaitOne(second * 10000);             
+                waitMessageBoxWindow.Dispatcher.Invoke(() => {
+                    waitMessageBoxWindow.StopTimer();
+                    waitMessageBoxWindow.Close();
+                });
+                return dialogResult;
+            });
+            return await task;
+        }
+        #endregion
+    }
+}
