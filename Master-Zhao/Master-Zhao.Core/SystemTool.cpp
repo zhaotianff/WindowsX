56d8302a6bb8283268a4093868d73883a4f607c5
zhaotianff <zhaotianff@163.com>
2022-06-22 12:17:32 +0800

    NtQuerySystemInformation

--- Master-Zhao/Master-Zhao.Core/SystemTool.cpp
+++ Master-Zhao/Master-Zhao.Core/SystemTool.cpp
@@ -1,28 +1,31 @@
 #include"SystemTool.h"
 #include<VersionHelpers.h>
 #include<functional>
+#include<Winternl.h>
 
 SYSTEMTIME GetUserLoginTime()
 {
+	SYSTEMTIME stSys;
+
 	HINSTANCE hInstance = LoadLibrary(L"ntdll.dll");
 	if (hInstance == NULL)
-		return SYSTEMTIME();
+		return stSys;
 
-	auto NtQuerySystemInformation = (funNtQuerySystemInformation)GetProcAddress(hInstance, "NtQuerySystemInformation");
+	funNtQuerySystemInformation NtQuerySystemInformation = (funNtQuerySystemInformation)GetProcAddress(hInstance,"NtQuerySystemInformation");
 
 	if (NtQuerySystemInformation)
 	{
 		SYSTEM_TIME_INFORMATION tInfo{};
-		auto ret = NtQuerySystemInformation(3, &tInfo, sizeof(tInfo), 0);
+		auto ret = NtQuerySystemInformation(SystemTimeOfDayInformation, &tInfo, sizeof(tInfo), 0);
 		FILETIME stFile = *(FILETIME*)&tInfo.liKeBootTime;
-		SYSTEMTIME stSys;
+
 		FileTimeToLocalFileTime(&stFile, &stFile);
 		FileTimeToSystemTime(&stFile, &stSys);
 
 		FreeLibrary(hInstance);
-		return stSys;
 	}
-	return SYSTEMTIME();
+	
+	return stSys;
 }
 
 void GetVersionNumbers(DWORD* pdwMajor, DWORD* pdwMinor, DWORD* pdwBuildNumber)
@@ -78,3 +81,91 @@ BOOL RegisterFastRunHotKey(HWND hwnd)
 
 	return bRet;
 }
+
+int GetCPUKernelCount()
+{
+	SYSTEM_INFO sinfo{};
+	GetSystemInfo(&sinfo);
+	return sinfo.dwNumberOfProcessors;
+}
+
+BOOL AdjustPrivilege()
+{
+	HANDLE hToken = NULL;
+	LUID uID; //Describes a local identifier for an adapter.
+	TOKEN_PRIVILEGES privValues;
+
+	BOOL bOpen = OpenProcessToken(GetCurrentProcess(), TOKEN_ADJUST_PRIVILEGES | TOKEN_QUERY, &hToken);
+	if (!bOpen)
+		return FALSE;
+
+	BOOL bLooked = LookupPrivilegeValue(NULL, SE_DEBUG_NAME ,&uID);
+
+	if (!bLooked)
+	{
+		CloseHandle(hToken);
+		hToken = NULL;
+	}
+
+	privValues.PrivilegeCount = 1;
+	privValues.Privileges[0].Luid = uID;
+	privValues.Privileges[0].Attributes = SE_PRIVILEGE_ENABLED;
+	//The AdjustTokenPrivileges function enables or disables privileges in the specified access token. 
+	//Enabling or disabling privileges in an access token requires TOKEN_ADJUST_PRIVILEGES access.
+	BOOL bAdjusted = AdjustTokenPrivileges(hToken, FALSE, &privValues, sizeof(privValues), NULL, NULL);
+
+	if (!bAdjusted)
+		return FALSE;
+
+	CloseHandle(hToken);
+	hToken = NULL;
+	return TRUE;
+}
+
+BOOL ForceDeleteFile(LPTSTR lpszFilePah)
+{
+	BOOL bResult = FALSE;
+	return bResult;
+
+	HINSTANCE hInstance = LoadLibrary(L"ntdll.dll");
+	if (hInstance == NULL)
+		return bResult;
+
+	funNtQuerySystemInformation NtQuerySystemInformation = (funNtQuerySystemInformation)GetProcAddress(hInstance, "NtQuerySystemInformation");
+
+	if (NtQuerySystemInformation)
+	{
+		int nCount = 0;
+		SYSTEM_PROCESS_INFORMATION* pSystemProcess = NULL;
+		DWORD dwBufferSize = 0xFFFFF;
+		pSystemProcess = (SYSTEM_PROCESS_INFORMATION*)malloc(dwBufferSize);
+		DWORD dwRet = NtQuerySystemInformation(SystemProcessInformation, pSystemProcess, dwBufferSize, &dwBufferSize);
+
+		if (dwRet != 0)
+		{
+			return bResult;
+		}
+
+		while (pSystemProcess->NextEntryOffset)
+		{
+			if (0 != pSystemProcess->UniqueProcessId)
+			{
+				//pSystemProcess->ImageName
+				//pSystemProcess->UniqueProcessId
+				//add to list
+			}
+
+			pSystemProcess = (SYSTEM_PROCESS_INFORMATION*)(((PUCHAR)pSystemProcess) + pSystemProcess->NextEntryOffset);
+		}
+
+		bResult = true;
+
+		if (pSystemProcess)
+			free(pSystemProcess);
+	}
+
+	return bResult;
+}
+
+
+
