5c385b51c3e85f9215018bffddb8336bf004fdf5
zhaotianff <zhaotianff@163.com>
2022-08-12 22:18:55 +0800

    menuitem style

--- Master-Zhao/Master-Zhao.Shell/StartMenu/Win98/Windows98.xaml.cs
+++ Master-Zhao/Master-Zhao.Shell/StartMenu/Win98/Windows98.xaml.cs
@@ -19,6 +19,7 @@ namespace Master_Zhao.Shell.StartMenu.Win98
     {
         private const double StartWidthRatio = 0.136d;
         private const double TitleWidthRatio = 0.113d;
+        private const double MenuItemHeightRatio = 0.0917d;
 
         public Windows98() : base()
         {
@@ -27,14 +28,27 @@ namespace Master_Zhao.Shell.StartMenu.Win98
 
         private void StartMenuWindowBase_Loaded(object sender, RoutedEventArgs e)
         {
+            LoadStartMenuItem();
+        }
 
-           
+        private void LoadStartMenuItem()
+        {
+            for(int i = 0;i<10;i++)
+            {
+                MenuItem item = new MenuItem();
+                item.Header = "Windows Update";
+                item.Height = 37;
+                item.Width = this.menu.Width;
+                this.menu.Items.Add(item);
+            }
         }
 
         public  override void SetStartMenuSize()
         {
             this.Width = SystemParameters.PrimaryScreenWidth * StartWidthRatio;
             this.grid_title.Width = this.Width * TitleWidthRatio;
+            this.grid_menu.Width = this.Width - this.grid_title.Width;
+            this.menu.Width = this.grid_menu.Width;
         }
     }
 }
