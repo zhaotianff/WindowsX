3200f82fc2a2623d0b28e34fd3c9dd97a54fedc1
zhaotianff <zhaotianff@163.com>
2022-09-21 17:26:10 +0800

    software background setting

---
+++ Master-Zhao/Master-Zhao.Shell/View/Setting/Pages/BackgroundSetting.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Master_Zhao.Shell.View.Setting.Pages
+{
+    /// <summary>
+    /// BackgroundSetting.xaml 的交互逻辑
+    /// </summary>
+    public partial class BackgroundSetting : Page
+    {
+        public BackgroundSetting()
+        {
+            InitializeComponent();
+        }
+
+        private void Setbackground_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Application.Current.MainWindow.Background = (sender as Rectangle).Fill;
+        }
+    }
+}
--- Master-Zhao/Master-Zhao.Shell/View/Setting/SettingPage.xaml.cs
+++ Master-Zhao/Master-Zhao.Shell/View/Setting/SettingPage.xaml.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using Master_Zhao.Shell.Infrastructure.Navigation;
+using Master_Zhao.Shell.View.Setting.Pages;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -16,16 +19,41 @@ namespace Master_Zhao.Shell.View.Setting
     /// <summary>
     /// SettingPage.xaml 的交互逻辑
     /// </summary>
-    public partial class SettingPage : Page
+    public partial class SettingPage : Page, IPageAction
     {
+        private ToggleButton toggleButton = null;
+        private BackgroundSetting backgroundSetting = new BackgroundSetting();
+
         public SettingPage()
         {
             InitializeComponent();
         }
 
+        public void ShowDefaultPage()
+        {
+            btn_BackgroundSettingClick(null, null);
+        }
+
+        public void Terminate()
+        {
+        }
+
         private void btnReturn_Click(object sender, RoutedEventArgs e)
         {
             (System.Windows.Application.Current.MainWindow as MainWindow).EndShowMenuAnimation();
+        }
+
+        private void btn_BackgroundSettingClick(object sender, RoutedEventArgs e)
+        {
+            this.frame.Content = backgroundSetting;
+        }
+
+        private void ToggleButton_Checked(object sender, RoutedEventArgs e)
+        {
+            if (toggleButton != null)
+                toggleButton.IsChecked = false;
+
+            toggleButton = sender as ToggleButton;
         }
     }
 }
