54823ca3af233ea5d185e90258402239ee1c9faf
zhaotianff <zhaotianff@163.com>
2022-09-26 17:18:48 +0800

    prepare shell tool

---
+++ Master-Zhao/Master-Zhao.Core/ShellTool.cpp
@@ -0,0 +1,12 @@
+#include"ShellTool.h"
+
+BOOL SetExplorerBackground(LPTSTR szPath, DWORD alpha, Enm_BackgroundPos pos)
+{
+    return 0;
+}
+
+VOID SetExplorerBackgroundParam(DWORD alpha, Enm_BackgroundPos pos)
+{
+    dw_Alpha = alpha;
+    enm_Pos = pos;
+}
