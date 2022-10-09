26a3a25557ff59ed87cafd65cc729ceb7c8b11fc
zhaotianff <zhaotianff@163.com>
2022-10-09 17:43:11 +0800

    temp commit

---
+++ Master-Zhao/Master-Zhao.Shell/StartMenu/Flat/FlatStartMenu.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Master_Zhao.Shell.StartMenu.Flat
+{
+    /// <summary>
+    /// FlatStartMenu.xaml 的交互逻辑
+    /// </summary>
+    public partial class FlatStartMenu : TianXiaTech.BlurWindow
+    {
+        public FlatStartMenu()
+        {
+            InitializeComponent();
+            this.Visibility = Visibility.Hidden;
+            Test();
+        }
+
+        private async void Test()
+        {
+            await Task.Delay(1000);
+
+            this.Left = 0;
+            this.Top = SystemParameters.WorkArea.Height - this.Height;
+
+
+            await Task.Delay(2000);
+            ShowMenu();
+        }
+
+        private void ShowMenu()
+        {
+            this.Visibility = Visibility.Visible;
+            DoubleAnimation showAnimation = new DoubleAnimation();
+            showAnimation.From = 0;
+            showAnimation.To = 1;
+            showAnimation.Duration = TimeSpan.FromMilliseconds(300);
+            this.BeginAnimation(OpacityProperty, showAnimation);
+        }
+
+        private void HideMenu()
+        {
+
+        }
+    }
+}
