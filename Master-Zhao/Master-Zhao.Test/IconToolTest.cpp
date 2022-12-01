c264c9c2cafe2d1636b3be17815eb7e944f2e908
zhaotianff <zhaotianff@163.com>
2022-12-01 17:03:35 +0800

    remove imgui test

--- Master-Zhao/Master-Zhao.Test/IconToolTest.cpp
+++
@@ -1,7 +0,0 @@
-#include"IconToolTest.h"
-
-void TestGetFileIcon()
-{
-	HICON icon = NULL;
-	ExtractFirstIconFromFile(L"C:\\Windows\\notepad.exe", TRUE, icon);
-}
--- Master-Zhao/Master-Zhao.Test/IconToolTest.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include"../Master-Zhao.Core/IconTool.h"
-#include"imgui/imgui.h"
-
-void TestGetFileIcon();
