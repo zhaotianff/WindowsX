6d12070d60f40d39215c6186107928c2abf8807c
zhaotianff <zhaotianff@163.com>
2023-12-21 17:31:36 +0800

    add mouse effect item control

--- Master-Zhao/Master-Zhao.Shell/View/Beautify/UserControls/MouseEffectItemControl.xaml.cs
+++ Master-Zhao/Master-Zhao.Shell/View/Beautify/UserControls/MouseEffectItemControl.xaml.cs
@@ -18,9 +18,70 @@ namespace Master_Zhao.Shell.View.Beautify.UserControls
     /// </summary>
     public partial class MouseEffectItemControl : UserControl
     {
+        public static readonly RoutedEvent OnOpenSettingEvent = EventManager.RegisterRoutedEvent("OnOpenSetting", RoutingStrategy.Bubble, typeof(RoutedEventHandler), typeof(MouseEffectItemControl));
+
+        public event RoutedEventHandler OnOpenSetting
+        {
+            add
+            {
+                AddHandler(OnOpenSettingEvent, value);
+            }
+            remove
+            {
+                RemoveHandler(OnOpenSettingEvent, value);
+            }
+        }
+
+        public static readonly RoutedEvent OnSetEvent = EventManager.RegisterRoutedEvent("OnSet", RoutingStrategy.Bubble, typeof(RoutedEventHandler), typeof(MouseEffectItemControl));
+
+        public event RoutedEventHandler OnSet
+        {
+            add
+            {
+                AddHandler(OnSetEvent, value);
+            }
+            remove
+            {
+                RemoveHandler(OnSetEvent, value);
+            }
+        }
+
+        public string Title
+        {
+            get => this.title.Content.ToString();
+            set => this.title.Content = value;
+        }
+
+        public string PreviewImage
+        {
+            set
+            {
+                this.image.Source = new Uri(value);
+                this.image.Play();
+            }
+        }
+
         public MouseEffectItemControl()
         {
             InitializeComponent();
+
+            this.image.LoadedBehavior = MediaState.Manual;
+            this.image.Volume = 0;
+        }
+
+        private void btn_Setting_Click(object sender, RoutedEventArgs e)
+        {
+            this.RaiseEvent(new RoutedEventArgs(OnOpenSettingEvent));
+        }
+
+        private void set_Click(object sender, RoutedEventArgs e)
+        {
+            this.RaiseEvent(new RoutedEventArgs(OnSetEvent));
+        }
+
+        private void image_MediaEnded(object sender, RoutedEventArgs e)
+        {
+            this.image.Play();
         }
     }
 }
