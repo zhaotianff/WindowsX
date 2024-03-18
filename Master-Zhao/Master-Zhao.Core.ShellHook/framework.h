060e47ebb28cd8ead638f3a3ee5882d29e2cdaae
zhaotianff <zhaotianff@163.com>
2024-03-18 17:37:29 +0800

    ready to apihook

--- Master-Zhao/Master-Zhao.Core.ShellHook/framework.h
+++ Master-Zhao/Master-Zhao.Core.ShellHook/framework.h
@@ -3,3 +3,8 @@
 #define WIN32_LEAN_AND_MEAN             // 从 Windows 头文件中排除极少使用的内容
 // Windows 头文件
 #include <windows.h>
+#include<comdef.h>
+#include<gdiplus.h>
+
+#include "include/MinHook.h"
+#include"ShellWindow.h"
